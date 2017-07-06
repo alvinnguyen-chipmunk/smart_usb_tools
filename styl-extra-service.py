2b72d9cc6f2f093f80838ec3dee3f453ae7d38fb
Alvin Nguyen <alvin.nguyen@styl.solutions>
2017-07-06 16:33:31 +0700

    Add source code file
    Signed-off-by: Alvin Nguyen <alvin.nguyen@styl.solutions>

---
+++ styl-extra-service.py
@@ -0,0 +1,12 @@
+##################################################################################################################################
+##      Styl-extra-service.py : Python script will to do:                                                                       ##
+##              - Detect USB plugin then mount it                                                                               ##
+##              - Search for Wifi pass, AGPS key, EMV configure file                                                            ##
+##              - Update information found                                                                                      ##
+##                                                                                                                              ##
+##       Create:    2017-07-06 14:30:00                                                                                         ##
+##       Modified:  -                                                                                                           ##
+##       Author:    Alvin Nguyen (alvin.nguyen@styl.solutions)                                                                  ##
+##       Copyright: STYL Solutions Pte. Lte.                                                                                    ##
+##                                                                                                                              ##
+##################################################################################################################################
